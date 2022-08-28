034d23b3460b75cfd962b307c85d02ef8395a440
Ryan Zmuda <ryanzmuda@gmail.com>
2022-08-28 18:15:52 -0400

    Lab 3 and Lab 4 CPS150

---
+++ CPS150/Lab_4.2/src/WhatIsThis.java
@@ -0,0 +1,11 @@
+import java.net.URL;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+public class WhatIsThis {
+    public static void main(String[] args) throws Exception {
+        URL imageLocation = new URL("http://horstmann.com/java4everyone/duke.gif");
+        JOptionPane.showMessageDialog(null, "Hello", "Title",
+            JOptionPane.PLAIN_MESSAGE, new ImageIcon(imageLocation));
+    }
+}
