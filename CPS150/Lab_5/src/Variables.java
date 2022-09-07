0a1986ea43da88900e295f6975ea00eeb97a5df5
Ryan Zmuda <ryanzmuda@gmail.com>
2022-09-07 16:09:29 -0400

    hw1, lab5

---
+++ CPS150/Lab_5/src/Variables.java
@@ -0,0 +1,36 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+		int one = 1; 					System.out.println("one: "+one);
+		double two = 2.0; 				System.out.println("two: "+two);
+		long three = 3843579837693768L; System.out.println("three: "+three);
+		float four = 88.99999F; 		System.out.println("four: "+four);
+		byte five = 0; 					System.out.println("five: "+five);
+		short six = 0; 					System.out.println("six: "+six);
+		char seven = 10000; 			System.out.println("seven: "+seven);
+		
+										System.out.println();
+		
+		one = 11; 						System.out.println("one: "+one);
+		two = 2.2; 						System.out.println("two: "+two);
+		three = 579837693768L; 			System.out.println("three: "+three);
+		four = 8448.99999234F; 			System.out.println("four: "+four);
+		five = 1; 						System.out.println("five: "+five);
+		six = 4; 						System.out.println("six: "+six);
+		seven = 20000; 					System.out.println("seven: "+seven);
+		
+										System.out.println();
+		
+		final int one1 = 1; 			System.out.println("one: "+one1);
+		final double two2 = 2.0; 		System.out.println("two: "+two2);
+		final long three3 = 384353768L; System.out.println("three: "+three3);
+		final float four4 = 88.99999F; 	System.out.println("four: "+four4);
+		final byte five5 = 0; 			System.out.println("five: "+five5);
+		final short six6 = 0; 			System.out.println("six: "+six6);
+		final char seven7 = 1738;	 	System.out.println("seven: "+seven7);
+	
+	}
+
+}
