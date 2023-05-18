76b0d01c4537fad277346882a2b9c69366976313
Yoyolick <ryanzmuda@gmail.com>
2023-05-18 10:56:06 -0400

    finished porting simpletron to C

--- INTERNSHIPS/RIVERSIDE/C_PRACTICE/projects/simpletron/simpletron.c
+++ INTERNSHIPS/RIVERSIDE/C_PRACTICE/projects/simpletron/simpletron.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 
 static int memory[100];
 static int accumulator = 0;
@@ -6,6 +7,48 @@ static int instructionRegister = 0;
 static int operationCode = 0;
 static int instructionCounter;
 static int operand = 0;
+
+// memory dump
+void dump(){
+    printf("\nREGISTERS:\n");
+
+    if(accumulator >= 0){
+        printf("accumulator:\t\t+%04d\n",accumulator);
+    }
+    else{
+        printf("accumulator:\t\t%05d\n",accumulator);
+    }
+
+    printf("instructionCounter:\t%02d\n",instructionCounter);
+
+    if(instructionRegister >= 0){
+        printf("instructionRegister:\t+%04d\n",instructionRegister);
+    }
+    else{
+        printf("instructionRegister:\t%05d\n",instructionRegister);
+    }
+
+    printf("operationCode:\t\t%02d\n",operationCode);
+
+    printf("operand:\t\t%02d\n\n",operand);
+
+
+    printf("MEMORY:\n");
+    printf("\t00\t01\t02\t03\t04\t05\t06\t07\t08\t09\t\n");
+    for(int i = 0; i < 10; i++){
+        printf("%02d\t",i*10);
+        for(int j = 0; j < 10; j++){
+            int current = memory[i * 10 + j];
+            if(current >= 0){
+                printf("+%04d\t",current);
+            }
+            else{
+                printf("%05d\t",current);
+            }
+        }
+        printf("\n");
+    }
+}
 
 int main() {
     const int READ = 10;
@@ -31,16 +74,152 @@ int main() {
     int inputting = 1;
     int instruction = 0;
 
-    while (inputting = 1){
+    // input loop
+
+    while (inputting == 1){
         int input;
         printf("%02d ? ",instruction);
-        scanf("%d",&input);
-        if(input <= 9999 && input >= -9999){ // TODO: needs rewritten does not work for all cases (letters). input validation is awful
+        if(scanf("%d",&input) != 1){
+            
+            // clear input buffer
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF) {}
+            
+            if (isalpha(c)) {
+                // printf("Invalid input. Please enter a numeric value.\n");
+                inputting = 0;
+            } else {
+                // printf("Invalid input. Please enter a valid value.\n");
+                inputting = 0;
+            }
+
+            continue;
+        }
+        if(input <= 9999 && input >= -9999){
             memory[instruction] = input;
         }
         else{
+            printf("Value out of range (-9999,9999).\n");
             continue;
         }
         instruction++;
     }
+
+    // running the program
+    printf("\n*** Program loading completed. ***\n");
+    dump();
+    printf("\n*** Program execution begins. ***\n");
+    
+    for(instructionCounter = 0; instructionCounter < 99; instructionCounter++){
+        instructionRegister = memory[instructionCounter];
+
+        operationCode = instructionRegister / 100;
+        operand = instructionRegister % 100;
+
+        if(operationCode == READ){
+            int valid = 0;
+            int input = 0;
+            while(valid != 1){
+                printf("\nEnter data for location %d: ",operand);
+                if(scanf("%d",&input) != 1){
+                
+                    // clear input buffer
+                    int c;
+                    while ((c = getchar()) != '\n' && c != EOF) {}
+                    
+                    if (isalpha(c)) {
+                        printf("Invalid input. Please enter a numeric value.\n");
+                        
+                    } else {
+                        printf("Invalid input. Please enter a valid value.\n");
+                        
+                    }
+
+                    continue;
+                }
+                if(input > 9999 || input < -9999){
+                    printf("Value out of range (-9999,9999).\n");
+                    continue;
+                }
+                else{
+                    valid = 1;
+                }
+            }
+            memory[operand] = input;
+        }
+        else if(operationCode == WRITE){
+            printf("\nData at location %d\n: ",operand);
+            if(memory[operand] >= 0){
+                printf("+%04d",memory[operand]);
+            }
+            else{
+                printf("%04d",memory[operand]);
+            }
+        }
+        else if(operationCode == LOAD){
+            accumulator = memory[operand];
+        }
+        else if(operationCode == STORE){
+            memory[operand] = accumulator;
+        }
+        else if(operationCode == ADD){
+            if(accumulator + memory[operand] > 9999 || accumulator + memory[operand] < -9999){
+                printf("\nERROR: ACCUMULATOR OVERFLOW\n");
+                break;
+            }
+            else{
+                accumulator += memory[operand];
+            }
+        }
+        else if(operationCode == SUBTRACT){
+            if(accumulator - memory[operand] > 9999 || accumulator - memory[operand] < -9999){
+                printf("\nERROR: ACCUMULATOR OVERFLOW\n");
+                break;
+            }
+            else{
+                accumulator -= memory[operand];
+            }
+        }
+        else if(operationCode == MULTIPLY){
+            if(accumulator * memory[operand] > 9999 || accumulator * memory[operand] < -9999){
+                printf("\nERROR: ACCUMULATOR OVERFLOW\n");
+                break;
+            }
+            else{
+                accumulator *= memory[operand];
+            }
+        }
+        else if(operationCode == DIVIDE){
+            if(memory[operand] == 0){
+                printf("ERROR: CANT DIVIDE BY ZERO");
+                break;
+            }
+            else{
+                accumulator /= memory[operand];
+            }
+        }
+        else if(operationCode == BRANCH){
+            instructionCounter = operand;
+        }
+        else if(operationCode == BRANCHNEG){
+            if(accumulator < 0){
+                instructionCounter = operand;
+            }
+        }
+        else if(operationCode == BRANCHZERO){
+            if(accumulator == 0){
+                instructionCounter = operand;
+            }
+        }
+        else if(operationCode == HALT){
+            break;
+        }
+        else{
+            printf("\n***ERROR: Unknown operation code***\n");
+            break;
+        }
+    }
+
+    dump(); // final memory dump
+
 }
